74f672e739dd66a277a2674660b8215001bd5048
Zaid <71690459+Zaid97-kai@users.noreply.github.com>
2024-01-28 14:00:27 +0300

    Fix #6

--- StudentCouncilTracker.Application/Entities/Events/Dto/EventDtoData.cs
+++ StudentCouncilTracker.Application/Entities/Events/Dto/EventDtoData.cs
@@ -3,6 +3,7 @@ using StudentCouncilTracker.Application.DynamicFields.Attributes;
 using StudentCouncilTracker.Application.DynamicFields.Enums;
 using StudentCouncilTracker.Application.Entities.Base.Dto;
 using StudentCouncilTracker.Application.Entities.EventTypes.Domain;
+using StudentCouncilTracker.Application.Entities.EventTypes.Dto.Combobox;
 using StudentCouncilTracker.Application.Entities.Interfaces.Haves;
 using StudentCouncilTracker.Application.Entities.Users.Domain;
 
@@ -19,7 +20,7 @@ public class EventDtoData : EntityDtoData, IHaveId
     public DynamicFieldValue<string>? Description { get; set; }
 
     [DynamicField(DynamicFieldType.EventType)]
-    public DynamicFieldValue<EventType>? EventType { get; set; }
+    public DynamicFieldValue<EventTypeDtoCombobox>? EventType { get; set; }
 
     [DynamicField(DynamicFieldType.CatalogUser)]
     public DynamicFieldValue<CatalogUser>? ResponsibleUser { get; set; }
