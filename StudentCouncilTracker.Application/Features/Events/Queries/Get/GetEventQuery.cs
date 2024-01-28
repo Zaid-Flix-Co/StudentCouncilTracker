e2dcc7e7a82dd2bacb6ba6a6a568cf85703f692e
Zaid <71690459+Zaid97-kai@users.noreply.github.com>
2024-01-28 13:09:50 +0300

    Fix #5

--- StudentCouncilTracker.Application/Features/Events/Queries/Get/GetEventQuery.cs
+++ StudentCouncilTracker.Application/Features/Events/Queries/Get/GetEventQuery.cs
@@ -9,11 +9,11 @@ namespace StudentCouncilTracker.Application.Features.Events.Queries.Get;
 
 public record GetEventQuery(ListFilter Filter) : IRequest<IOperationResult<ListDto>>;
 
-public class GetEventQueryHandler(IEventRepository catalogEventRepository) : IRequestHandler<GetEventQuery, IOperationResult<ListDto>>
+public class GetEventQueryHandler(IEventRepository repository) : IRequestHandler<GetEventQuery, IOperationResult<ListDto>>
 {
     public async Task<IOperationResult<ListDto>> Handle(GetEventQuery request, CancellationToken cancellationToken)
     {
-        var result = OperationResult.CreateResult(await catalogEventRepository.GetList(request.Filter));
+        var result = OperationResult.CreateResult(await repository.GetList(request.Filter));
         return result;
     }
 }
