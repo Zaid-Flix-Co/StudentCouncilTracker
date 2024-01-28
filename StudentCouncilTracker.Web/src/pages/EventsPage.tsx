7d436320622efdf29c32d8f6edbdf7e4e61c026c
Zaid <zaid97abyi@gmail.com>
2024-01-28 12:14:14 +0300

    Add web

---
+++ StudentCouncilTracker.Web/src/pages/EventsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventsPage: React.FC = () => {
+  return <div>Содержимое страницы Справочник мероприятий</div>;
+};
+
+export default EventsPage;
---
+++ StudentCouncilTracker.Web/src/pages/UsersPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UsersPage: React.FC = () => {
+  return <div>Содержимое страницы Справочник пользователей</div>;
+};
+
+export default UsersPage;
