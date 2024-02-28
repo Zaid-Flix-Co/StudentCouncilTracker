3ab52ea8592658c8643d19f50e704b9e45b6cf11
Zaid <71690459+Zaid97-kai@users.noreply.github.com>
2024-02-28 09:59:46 +0300

    Delete react app

--- StudentCouncilTracker.Web/src/pages/EventsPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventsPage: React.FC = () => {
-  return <div>Содержимое страницы Справочник мероприятий</div>;
-};
-
-export default EventsPage;
--- StudentCouncilTracker.Web/src/pages/UsersPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UsersPage: React.FC = () => {
-  return <div>Содержимое страницы Справочник пользователей</div>;
-};
-
-export default UsersPage;
