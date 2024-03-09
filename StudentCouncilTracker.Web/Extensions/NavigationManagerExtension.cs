599eabb8350c0959c931e808337d1dd70eb548b3
Zaid97-kai <71690459+Zaid97-kai@users.noreply.github.com>
2024-03-09 11:16:29 +0300

    Fix route 11

--- StudentCouncilTracker.Web/Extensions/NavigationManagerExtension.cs
+++ StudentCouncilTracker.Web/Extensions/NavigationManagerExtension.cs
@@ -4,7 +4,7 @@ namespace StudentCouncilTracker.Web.Extensions;
 
 public static class NavigationManagerExtension
 {
-    private const string SubDomain = "skt";
+    private const string SubDomain = "/skt";
 
     public static void NavigateToPage(this NavigationManager navigationManager, string url, bool forceLoad = false)
     {
