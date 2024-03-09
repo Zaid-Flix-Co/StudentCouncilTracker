626252c5d87fb13780298f49e3da9b433232d42c
Zaid97-kai <71690459+Zaid97-kai@users.noreply.github.com>
2024-03-09 11:00:16 +0300

    Fix route 9

---
+++ StudentCouncilTracker.Web/Pages/Login.razor.js
@@ -0,0 +1,6 @@
+window.setCookie = (name, value, minutes) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
