6d115a238a835465875196ccee5e4391515f52e3
Zaid97-kai <71690459+Zaid97-kai@users.noreply.github.com>
2024-03-12 23:56:47 +0300

    Add wpf menu

--- StudentCouncilTracker.Desktop.Admin/MainWindow.xaml.cs
+++ StudentCouncilTracker.Desktop.Admin/MainWindow.xaml.cs
@@ -1,38 +1,54 @@
 ﻿using System.Windows;
+using Microsoft.IdentityModel.Tokens;
 using StudentCouncilTracker.Application.Entities.Users.Dto;
 using StudentCouncilTracker.Desktop.Admin.Services.Catalogs.Auth;
 using StudentCouncilTracker.Desktop.Admin.Services.Catalogs.Users;
 using StudentCouncilTracker.Desktop.Admin.Services.UserProviders;
+using StudentCouncilTracker.Desktop.Admin.Windows;
 
 namespace StudentCouncilTracker.Desktop.Admin;
 
 public partial class MainWindow : Window
 {
-    private AuthService AuthService { get; set; }    
+    private readonly AuthService _authService;
 
-    private CatalogUserService UserService { get; set; }    
+    private readonly CatalogUserService _userService;
 
-    private IUserProvider UserProvider { get; set; }    
-    
-    private CatalogUserDto UserDto { get; set; }
+    private readonly IUserProvider _userProvider;
 
-    public MainWindow(AuthService authService, CatalogUserService userService, IUserProvider userProvider)
+    private readonly MenuWindow _menuWindow;
+
+    private CatalogUserDto UserDto { get; set; } = null!;
+
+    public MainWindow(AuthService authService, CatalogUserService userService, IUserProvider userProvider, MenuWindow menuWindow)
     {
-        AuthService = authService;
-        UserService = userService;
-        UserProvider = userProvider;
+        _authService = authService;
+        _userService = userService;
+        _userProvider = userProvider;
+        _menuWindow = menuWindow;
+
         InitializeComponent();
     }
 
     private async void Login_Click(object sender, RoutedEventArgs e)
     {
-        UserDto = (await UserService.GetEmptyAsync()).Value;
+        UserDto = (await _userService.GetEmptyAsync()).Value;
 
         UserDto.Data.Email!.Value = txtUsername.Text;
         UserDto.Data.Password!.Value = txtPassword.Password;
 
-        var token = await AuthService.LoginAsync(UserDto);
+        var token = await _authService.LoginAsync(UserDto);
 
-        UserProvider.ParseJwt(token.Value.AccessToken);
+        if(token.Value.AccessToken.IsNullOrEmpty())
+        {
+            MessageBox.Show("Ошибка входа");
+            return;
+        }
+
+        _userProvider.ParseJwt(token.Value.AccessToken);
+
+        MenuWindow.MainWindow = this;
+        _menuWindow.Show();
+        Hide();
     }
 }
